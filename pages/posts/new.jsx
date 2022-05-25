d5ff3ee74984a022f3f59b3ac3b48db5d9f6fb87
Zulfadhli Zakari <zufadhlizakari@gmail.com>
2022-05-25 10:46:39 +0800

    feat: add create post function

--- pages/posts/new.jsx
+++ pages/posts/new.jsx
@@ -1,11 +1,39 @@
+import {useRouter} from 'next/router'
+import { useForm } from 'react-hook-form'
+import slugify from '@sindresorhus/slugify'
+import { useUser } from '@lib/use-user'
+import { db } from '@lib/firebase'
+import { doc, setDoc } from 'firebase/firestore'
+
 export default function NewPostPage() {
+    const router = useRouter()
+  const { user, username } = useUser()
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = handleSubmit(async (data) => {
+    const post = {
+      uid: user.uid,
+      username: username,
+      title: data.title,
+      slug: slugify(data.title),
+      content: data.content,
+    }
+
+    const postRef = doc(db, 'users', user.uid, 'posts', post.slug)
+    await setDoc(postRef, { ...post }, { merge: true })
+    router.push('/posts')
+  })
   return (
     <>
       <section className='section'>
         <div className='container-sm'>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-              <input className='w-full px-3 py-2' type='text' placeholder='title' />
+              <input
+                className='w-full px-3 py-2'
+                type='text'
+                placeholder='title'
+                {...register('title')}
+              />
             </div>
             <div className='pt-2'>
               <textarea
@@ -14,6 +42,7 @@ export default function NewPostPage() {
                 id=''
                 cols='30'
                 rows='10'
+                {...register('content')}
               ></textarea>
             </div>
             <div className='pt-4 text-right'>
