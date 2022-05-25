1462dde0735d7906c8b03fc9e8a104d16409c448
Zulfadhli Zakari <zufadhlizakari@gmail.com>
2022-05-25 10:47:06 +0800

    feat: add individual post page

---
+++ pages/posts/[slug].js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '@lib/firebase'
+import { useAuth } from '@lib/use-auth'
+
+export default function PostPage() {
+  const { user } = useAuth()
+  const router = useRouter()
+  const { slug } = router.query
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    return () => getPost()
+  }, [])
+
+  const getPost = async () => {
+    const docRef = doc(db, 'users', user.uid, 'posts', slug)
+    const docSnap = await getDoc(docRef)
+    setPost(null)
+    if (docSnap.exists()) {
+      console.log('Document data:', docSnap.data())
+      setPost(docSnap.data())
+    } else {
+      console.log('No such document!')
+    }
+  }
+  return (
+    <>
+      <section className='section'>
+        <div className='container'>
+          {post && (
+            <>
+              <div className='p-4 bg-white rounded-lg shadow'>
+                <div>
+                  <h1 className='text-4xl font-bold tracking-tight uppercase'>{post.title}</h1>
+                  <p className='text-sm text-slate-700'>posted by {post.username}</p>
+                </div>
+                <div className='pt-8'>
+                  <p className='text-lg'>{post.content}</p>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </section>
+    </>
+  )
+}
